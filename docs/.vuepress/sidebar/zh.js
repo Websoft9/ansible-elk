3d5e6f2cc20f3cf06f17b7bcc48a83b2e71cb8ea
chendelin1982 <chendelin1982@126.com>
2021-07-29 15:19:50 +0800

    docs

--- docs/.vuepress/sidebar/zh.js
+++ docs/.vuepress/sidebar/zh.js
@@ -16,6 +16,7 @@ module.exports = [
     title: '实践指南',
     collapsable: false,
     children: [
+      '/zh/solution-study',
       '/zh/solution-smtp',
       '/zh/solution-https',
       '/zh/solution-cli',
@@ -30,7 +31,6 @@ module.exports = [
     collapsable: false,
     children: [
       '/zh/admin-services',
-      '/zh/admin-mysql',
       ['https://support.websoft9.com/docs/linux/zh/webs-nginx.html','Nginx'],
       ['https://support.websoft9.com/docs/linux/zh/admin-practices.html','Linux'],
     ]
--- docs/zh/stack-accounts.md
+++ docs/zh/stack-accounts.md
@@ -6,15 +6,10 @@
 
 下面列出可能需要用到的几组账号密码：
 
-## ELK
+## Kibana
 
-* 管理员账号: `admin`
-* 管理员密码: `admin` 或 存储在您的服务器中的文件中 */credentials/password.txt*  
-
-## MySQL
-
-* 管理员账号：*`root`*
-* 管理员密码：存储在您的服务器中的文件中 */credentials/password.txt*  
+* 管理员账号: `elastic`
+* 管理员密码: 存储在您的服务器中的文件中 */credentials/password.txt*  
 
 ## Linux
 
